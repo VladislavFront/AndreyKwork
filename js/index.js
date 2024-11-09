da43c44adad1f2925eac5c71d4a5b6d935a12683
Vladislav <pishanskijvladislav@gmail.com>
2024-11-09 22:40:48 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-const contactForm = document.getElementById('contactForm')
+emailjs.sendForm('service_26i75ls', 'template_dyb30wo', '#contactForm', '1xQx1IAwgwD7NzEo4').then(
+  (response) => {
+    console.log('SUCCESS!', response.status, response.text);
+  },
+  (error) => {
+    console.log('FAILED...', error);
+  },
+);
 
-const sendEmail = (event) => {
-  event.preventDefault()
-
-  emailjs.sendForm('serviceid', 'template_dyb30wo', '#contactForm', '1xQx1IAwgwD7NzEo4')
-    .then(() => {    
-      contactForm.reset()
-    })
-}
-
-contactForm.addEventListener('submit', sendEmail)
+// template_dyb30wo
